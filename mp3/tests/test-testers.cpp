0c55574d5f9dfd6409f4b653ee4b948cd3d40063
Nikhil Sheoran <sheoran2@illinois.edu>
2021-09-15 16:50:58 -0500

    Fix tester labels

--- mp3/tests/test-testers.cpp
+++ mp3/tests/test-testers.cpp
@@ -14,7 +14,7 @@ TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
   REQUIRE(result->status == 1);
 }
 
-TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
+TEST_CASE("tester2", "[weight=1][part=5][suite=week2]") {
   system("make -s");
   system("./mstats tests/testers_exe/tester2 evaluate");
   mstats_result * result = read_mstats_result("mstats_result.txt");
@@ -22,7 +22,7 @@ TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
   REQUIRE(result->status == 1);
 }
 
-TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
+TEST_CASE("tester3", "[weight=1][part=5][suite=week2]") {
   system("make -s");
   system("./mstats tests/testers_exe/tester3 evaluate");
   mstats_result * result = read_mstats_result("mstats_result.txt");
@@ -30,7 +30,7 @@ TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
   REQUIRE(result->status == 1);
 }
 
-TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
+TEST_CASE("tester4", "[weight=1][part=5][suite=week2]") {
   system("make -s");
   system("./mstats tests/testers_exe/tester4 evaluate");
   mstats_result * result = read_mstats_result("mstats_result.txt");
@@ -38,7 +38,7 @@ TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
   REQUIRE(result->status == 1);
 }
 
-TEST_CASE("tester1", "[weight=1][part=5][suite=week2]") {
+TEST_CASE("tester5", "[weight=1][part=5][suite=week2]") {
   system("make -s");
   system("./mstats tests/testers_exe/tester5 evaluate");
   mstats_result * result = read_mstats_result("mstats_result.txt");
