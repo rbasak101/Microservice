4f8430282d832ef5c9a42b035a58fc37319e39d0
Wade Fagen-Ulmschneider <waf@illinois.edu>
2021-10-08 11:32:36 -0500

    mp6 release

---
+++ mp6/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+# Route for "/" for a web-based interface to this micro-service:
+@app.route('/')
+def index():
+  from flask import render_template
+  return render_template("index.html")
+
+# Extract a hidden "uiuc" GIF from a PNG image:
+@app.route('/extract', methods=["POST"])
+def extract_hidden_gif():
+  from flask import send_file, request
+
+  # ...your code here...
