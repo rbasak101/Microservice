eb1cea802b7fdc6f6bba582e23a81c5f7ee8e2d8
Ron <basak4@illinois.edu>
2021-11-26 16:12:16 -0500

    Fixed output and input matching IM

--- project-MIX/middleware/app.py
+++ project-MIX/middleware/app.py
@@ -13,6 +13,77 @@ class Node:
   def print_nodeIMID(self):
     print(self.all_data["IMID"])
   
+  def getIMID(self):
+    return self.all_data["IMID"]
+  
+  def getInput(self):
+    return self.all_data["input"]
+  
+  def getOutput(self):
+    return self.all_data["output"]
+  
+
+class Graph: # mainly used for adding and removing 
+  adj_dict = {} # representation of adj list
+
+  def checkSubset(self, small):
+    for key in self.adj_dict:
+      # if key.getIMID() == small.getIMID():
+      #   return True
+      if small in self.adj_dict:
+        return True
+    return False
+  
+  def add_node(self, node):
+    #print("inside add_node")
+    node.print_nodeIMID()
+
+    if self.checkSubset(node) == False:
+      if node.all_data["input"] == "GPS":
+        self.adj_dict[node] = []
+        #print("added root")
+
+        for vertex in self.adj_dict.keys(): # possibly add another root and another lower node could utilize its output
+          if vertex.all_data["input"] == node.all_data["output"]:
+            self.adj_dict[vertex].append(node)
+      else:
+        flag = False
+        
+        for vertex in list(self.adj_dict.keys()):
+          if vertex.all_data["output"] == node.all_data["input"] or node.all_data["input"] in vertex.all_data["output"]:
+            #print("for lower IM")
+            #print(vertex.getIMID(), node.getIMID())
+            flag = True
+            self.adj_dict[node] = [vertex]
+
+        if flag == False:
+          print("Can not add because input and output do not match")
+        else:
+          print("Added")
+    else:
+      print("Already exists")
+    print("add_node() finished \n")
+
+  def remove_node(self, node): # takes in IMID instead of full node --> fails because key takes in node not IMID
+    print("entered remove function")
+    removed = self.adj_dict.pop(node, "Node (IM) does not exist")
+    print("removed key is")
+    node.print_nodeIMID()
+    for key, value in list(self.adj_dict.items()):
+      if len(value) == 1 and value[0].getIMID() == node.getIMID():
+        print("found another node to be deleted")
+        self.remove_node(key)
+  
+  def print_graph(self):
+    print("start of graph printing")
+    #print(self.adj_dict.items())
+    for key, value in self.adj_dict.items():
+      print(key.getIMID(), end=":")
+      for IM in value:
+        print(IM.getIMID())  
+      print(" ", end = "\n")  
+    
+
 
 # Route for "/" (frontend):
 @app.route('/')
@@ -20,11 +91,14 @@ def index():
   return render_template("index.html")
 
 
-
+graph = Graph()
 # Route for "/MIX" (middleware):
 @app.route('/MIX', methods=["POST"])
 def POST_weather():
+  print("Start of MIX")
   frontend = {}
+  graph.adj_dict.clear()
+  graph.print_graph()
   coordinates = request.form["location"]
   print(coordinates, type(coordinates))
 
@@ -43,51 +117,42 @@ def POST_weather():
   except:
     return "Error: Missing other coordinate", 400
 
-  server_url = os.getenv("IM5049_url") 
-  r = requests.get(f'{server_url}/{lat}/{long}')
-  data = r.json()
-  print(data)  
-
-  frontend = {**data, **frontend}
-
-  a,b,c = server_url.split(":")
-  IMID = c[0: len(c)-1]
-
-
-  root = Node(f'{server_url}/{lat}/{long}')
-  #print("root", root.all_data["country"])
-
   url_file = open("url.txt", "r")
   num_lines = sum(1 for line in open('url.txt'))
   for i in range(num_lines):
+
     print("i is", i)
     line = url_file.readline()
     line.rstrip()
-    print(line, type(line))
+    print(line)
+    link, input, output, hierarchy, parameters = line.split("|") 
+    if input == "GPS":
+      url = link + str(lat)+ "/" + str(long)
+      print("Line 133", url)
+      root = Node(url)
+      graph.add_node(root)
+      frontend = {**root.all_data, **frontend}
+      continue
 
-    if i >= 1:
-      link, input, output, hierarchy, parameters = line.split("|") 
-      print("Printing", root.all_data) 
-      print("input is", input)
-      if input in root.all_data["output"]:
-        url = link + root.all_data[input]
-        print(url)
-      nodeX = Node(url)
-      nodeX.print_nodeIMID()
-      frontend = {**nodeX.all_data, **frontend}
-    print("\n")
-    print("i is currently", i, num_lines)
+    for key in graph.adj_dict: # Find node with right output, input match
+      print("Line 142", key.all_data)
+      if input in key.getOutput():
+        url = link + key.all_data[input]
+        print("Line 145", url)
+        break
+
+    nodeX = Node(url)
+    nodeX.print_nodeIMID()
+    graph.add_node(nodeX)
+
+    frontend = {**nodeX.all_data, **frontend}
+
     if not line or i >= num_lines:
       break
   url_file.close()
-  print("j")
+
   del frontend["owner"], frontend["input"], frontend["output"], frontend["IMID"]
   print("printing final", frontend)
 
   return jsonify(frontend), 200
-  # return jsonify({"input": data["input"],
-  #                 "output": data["output"],
-  #                 "IMID": IMID,
-  #                 "city": data["city"],
-  #                 "country": data["country"]}), 200
 
