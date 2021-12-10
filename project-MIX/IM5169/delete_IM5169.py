d3eb81ca01c56ddce9a81901035d06ca28b24844
Basak <basak4@fa21-cs240-006.cs.illinois.edu>
2021-12-10 17:44:06 -0600

    Delete functions

---
+++ project-MIX/IM5169/delete_IM5169.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+import requests
+
+class_middleware = "http://cs240-adm-01.cs.illinois.edu:5000/microservice"
+
+my_ip = "http://172.22.150.7"
+
+data = {
+    "port" : "5169",
+    "ip" : my_ip,
+}
+
+status = requests.delete(class_middleware, json = data)
+print(status)
+print("Finished")
