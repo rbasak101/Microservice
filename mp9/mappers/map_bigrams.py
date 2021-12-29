e90120c94dedbc9b7cdabb2bbc528e8940037a5a
rbasak101 <60297449+rbasak101@users.noreply.github.com>
2021-12-29 17:07:52 -0500

    Delete mp9 directory

--- mp9/mappers/map_bigrams.py
+++
@@ -1,6 +0,0 @@
-import re
-from collections import Counter
-# map function:
-def map(filename):
-    words = re.findall('\w+', open(filename).read().lower())
-    return Counter(zip(words,words[1:])) #stackoverflow
--- mp9/reducers/reduce_bigrams.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-# reduce function:
-def reduce(left, right):
-
-  A = Counter(left)
-  B = Counter(right)
-  
-  return A+B
